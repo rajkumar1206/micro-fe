f1fc0da12fe53c8e877ced0314fb1b7cf4c1c851
Rajkumar R <rajkumarbaba12062000@gmail.com>
2022-12-20 16:41:28 +0530

    v1

---
+++ README.md
@@ -0,0 +1,1 @@
+sample application for micro frontend arch
---
+++ login/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+export default function App() {
+  return (
+    <div className="container">
+      <h2>Login</h2>
+    </div>
+  );
+}
+ReactDOM.render(<App />, document.getElementById("app"));
